5cd74895f036d8d7c22c6ef4f35f2fe597f43e73
Rajashekar Varkala <raj@mywavia.com>
2023-02-08 16:30:49 +0530

    adding vyper config

--- main.py
+++ main.py
@@ -1,12 +1,35 @@
 """docs:  http://127.0.0.1:8000/docs"""
+import logging
+import logging.config
+
 import uvicorn
 from fastapi import FastAPI
+from vyper import v
 
 from api.ctrls import users
 
-app = FastAPI()
-app.include_router(users.router)
+
+def setup_vyper() -> None:
+    v.set_config_type("yaml")
+    v.set_config_name("config")
+    v.add_config_path(".")
+    v.read_in_config()
+    v.automatic_env()
+    logging.config.dictConfig(v.get("LOGGING_CONF"))
+
+
+def main() -> None:
+    setup_vyper()
+
+    app = FastAPI()
+    app.include_router(users.router)
+
+    # Logging init
+    logger = logging.getLogger(__name__)
+    logger.info("Starting server...")
+
+    uvicorn.run(app)
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app")
+    main()
