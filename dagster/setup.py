9841e8beb8795e387892e83c845f55da5ee76962
Rajashekar Varkala <raj@mywavia.com>
2023-02-23 16:16:42 +0530

    remove unnecessary stuff

--- dagster/setup.py
+++ dagster/setup.py
@@ -1,4 +1,4 @@
-from setuptools import find_packages, setup
+from setuptools import setup
 
 install_requires = ["dagster", "dagster-docker"]
 
@@ -6,10 +6,5 @@ install_requires = ["dagster", "dagster-docker"]
 setup(
     name="valmi-repo",
     install_requires=install_requires,
-    use_scm_version=True,
-    setup_requires=["setuptools_scm"],
-    packages=find_packages(where="valmi_repo"),
-    package_dir={"": "valmi_repo"},
-    include_package_data=True,
     extras_require={"dev": ["flake8", "black", "mypy"]},
 )
