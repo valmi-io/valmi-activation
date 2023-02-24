81e8e7548d84a99cc69fa440b9eaa154b10e23d2
Rajashekar Varkala <raj@mywavia.com>
2023-02-24 16:42:41 +0530

    dry

--- dagster/valmi_repo/__init__.py
+++ dagster/valmi_repo/__init__.py
@@ -10,6 +10,7 @@ CONFIG_DIR = "config"
 CATALOG_DIR = "catalog"
 SHARED_DIR = "/tmp/shared_dir"
 APP = "valmi"
+REPO_DIR = "repo"
 
 
 # read the dagster jobs from storage
@@ -21,53 +22,38 @@ def get_dagster_jobs():
     # shutil.move(join(SHARED_DIR, APP, "repo", GENERATED_DIR), join(dirname(__file__), GENERATED_DIR))
 
 
+def get_jobs_schedules_from_py_files(files: list[str]):
+    imports = [basename(f)[:-3] for f in files if isfile(f) and not f.endswith("__init__.py")]
+
+    module_specs = [
+        importlib.util.spec_from_file_location(mod, join(SHARED_DIR, APP, REPO_DIR, GENERATED_DIR, f"{mod}.py"))
+        for mod in imports
+    ]
+    modules = [importlib.util.module_from_spec(module_spec) for module_spec in module_specs]
+
+    for idx, module_spec in enumerate(module_specs):
+        module_spec.loader.exec_module(modules[idx])
+
+    jobs = [getattr(mod, "job") for mod in modules]
+    schedules = [getattr(mod, "schedule") for mod in modules]
+
+    return {
+        "jobs": dict(zip([f"{file}".capitalize() for file in imports], jobs)),
+        "schedules": dict(zip([f"{file}_schedule".capitalize() for file in imports], schedules)),
+    }
+
+
 @repository
 def valmi_repo():
     os.system("printenv")
     if "DAGSTER_RUN_JOB_NAME" in os.environ:
-        files = [join(SHARED_DIR, APP, "repo", GENERATED_DIR, f"{os.environ['DAGSTER_RUN_JOB_NAME']}.py")]
-        imports = [basename(f)[:-3] for f in files if isfile(f) and not f.endswith("__init__.py")]
-
-        module_specs = [
-            importlib.util.spec_from_file_location(mod, join(SHARED_DIR, APP, "repo", GENERATED_DIR, f"{mod}.py"))
-            for mod in imports
-        ]
-        modules = [importlib.util.module_from_spec(module_spec) for module_spec in module_specs]
-
-        for idx, module_spec in enumerate(module_specs):
-            module_spec.loader.exec_module(modules[idx])
-
-        jobs = [getattr(mod, "job") for mod in modules]
-        schedules = [getattr(mod, "schedule") for mod in modules]
-
-        return {
-            "jobs": dict(zip([f"{file}".capitalize() for file in imports], jobs)),
-            "schedules": dict(zip([f"{file}_schedule".capitalize() for file in imports], schedules)),
-        }
-
+        # return only one job here
+        files = [join(SHARED_DIR, APP, REPO_DIR, GENERATED_DIR, f"{os.environ['DAGSTER_RUN_JOB_NAME']}.py")]
+        return get_jobs_schedules_from_py_files(files)
     else:
         get_dagster_jobs()
 
         import glob
 
-        files = glob.glob(join(SHARED_DIR, APP, "repo", GENERATED_DIR, "*.py"))
-
-        imports = [basename(f)[:-3] for f in files if isfile(f) and not f.endswith("__init__.py")]
-        module_specs = [
-            importlib.util.spec_from_file_location(mod, join(SHARED_DIR, APP, "repo", GENERATED_DIR, f"{mod}.py"))
-            for mod in imports
-        ]
-        modules = [importlib.util.module_from_spec(module_spec) for module_spec in module_specs]
-
-        for idx, module_spec in enumerate(module_specs):
-            module_spec.loader.exec_module(modules[idx])
-
-        # modules = [importlib.import_module(f"{GENERATED_DIR}.{mod}") for mod in imports]
-
-        jobs = [getattr(mod, "job") for mod in modules]
-        schedules = [getattr(mod, "schedule") for mod in modules]
-
-        return {
-            "jobs": dict(zip([file.capitalize() for file in imports], jobs)),
-            "schedules": dict(zip([f"{file}_schedule".capitalize() for file in imports], schedules)),
-        }
+        files = glob.glob(join(SHARED_DIR, APP, REPO_DIR, GENERATED_DIR, "*.py"))
+        return get_jobs_schedules_from_py_files(files)
