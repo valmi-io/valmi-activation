d62da1d79011457f324ec577edb25916f311b113
Rajashekar Varkala <raj@mywavia.com>
2023-03-09 22:22:48 +0530

    samples api stub

--- src/api/routers/syncs.py
+++ src/api/routers/syncs.py
@@ -1,10 +1,11 @@
 from datetime import datetime
 import logging
-from typing import List
+from typing import Any, List
 
 from fastapi import Depends
 
 from fastapi.routing import APIRouter
+from pydantic import UUID4, Json
 from vyper import v
 
 from metastore import models
@@ -29,4 +30,17 @@ async def get_sync_runs(
     limit: int = 0,
     sync_runs_service: SyncRunsService = Depends(get_sync_runs_service),
 ) -> List[models.SyncRun]:
-    return sync_runs_service.list()
+    ## MAJOR ITEM
+    pass
+    # return sync_runs_service.list()
+
+
+@router.get("/sync/{sync_id}/run/{run_id}/samples/{connector_id}", response_model=Json[Any])
+async def get_run_samples(
+    sync_id: UUID4,
+    run_id: UUID4,
+    sync_runs_service: SyncRunsService = Depends(get_sync_runs_service),
+) -> Json[Any]:
+    ## MAJOR ITEM
+    pass
+    # return sync_runs_service.list()
