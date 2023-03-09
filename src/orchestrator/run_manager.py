555f31311c8cd8a3033e977404e314e0b0a9f05d
Rajashekar Varkala <raj@mywavia.com>
2023-03-09 22:25:11 +0530

    TODO

--- src/orchestrator/run_manager.py
+++ src/orchestrator/run_manager.py
@@ -1,8 +1,6 @@
-# Launches jobs on dagster according to the schedule
+# Provide API for the connectors to save source and destination checkpoints
+
 # Handles failures and retries
-# Be on top of job runs and their status. Store state in the db.
-# Provide API for the frontend to query the status of the runs
-# Provide API for the frontend to save source and destination checkpoints
 # Provide Pull API for Synchronising the source and destination. And even sources and destinations ask whether to fail or continue.
 # Listen to Sources and Destinations for Control Messsages Like Oauth refreshes| Destination unreachable | Source unreachable
 # OAUTH refreshes should be done by the api Server (its responsibility is crendential management & configuration). This server is responsible for (job run & meta data management).
