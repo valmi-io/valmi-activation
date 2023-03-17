3eb2e18458a2e0bbcba02808f2cd8d4a6cece7bd
Rajashekar Varkala <raj@mywavia.com>
2023-03-17 17:57:52 +0530

    initialise renamed & tick interval for datastorecleaner

--- src/datastore/datastore_cleaner.py
+++ src/datastore/datastore_cleaner.py
@@ -60,3 +60,4 @@ class DatastoreCleanerThread(threading.Thread):
                 time.sleep(v.get_int("DATASTORE_CLEANER_SLEEP_TIME") or 60)
             except Exception:
                 logger.exception("Error while cleaned datastore")
+                time.sleep(v.get_int("DATASTORE_CLEANER_SLEEP_TIME") or 60)
