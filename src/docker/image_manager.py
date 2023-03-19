03a04f80439caff114f94a96e0d0d682d9300673
Rajashekar Varkala <raj@mywavia.com>
2023-03-19 18:25:10 +0530

    image pull manager update

--- src/docker/image_manager.py
+++ src/docker/image_manager.py
@@ -1,6 +1,9 @@
 import logging
+import os
 import threading
-
+import time
+import requests
+from requests.auth import HTTPBasicAuth
 from vyper import v
 
 logger = logging.getLogger(v.get("LOGGER_NAME"))
@@ -38,9 +41,26 @@ class DockerImageWarmupThread(threading.Thread):
         while not self.exitFlag:
             try:
                 logger.info("Pulling all Docker Container images")
-                # get all connectors from db
-                # issue docker pull for each connector
-                pass
+                resp = requests.get(
+                    f"http://{v.get('APP_BACKEND')}:{v.get('APP_BACKEND_PORT')}/api/v1/superuser/connectors/",
+                    timeout=v.get("HTTP_REQ_TIMEOUT"),
+                    auth=HTTPBasicAuth(v.get("ADMIN_EMAIL"), v.get("ADMIN_PASSWORD")),
+                )
+                connector_roles = ["SRC", "DEST"]
+
+                json_resp = resp.json()
+                for role in connector_roles:
+                    for typ in json_resp[role]:
+                        img = typ["docker_image"]
+                        tag = typ["docker_tag"]
+                        os.system(
+                            f"docker inspect --type=image {img}:{tag} >> /dev/null; \
+                                ret_val=$?; \
+                                if [ $ret_val -ne 0 ]; \
+                                    then docker pull {img}:{tag} ;\
+                                fi"
+                        )
+                time.sleep(v.get_int("DOCKER_IMAGE_MANAGER_SLEEP_TIME") or 60)
             except Exception:
                 logger.exception("Error while pulling images")
-            self.exitFlag = True
+                time.sleep(v.get_int("DOCKER_IMAGE_MANAGER_SLEEP_TIME") or 60)
