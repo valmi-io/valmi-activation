532a8de95cea0c104596b75f3a7ee93f51d69e7b
Rajashekar Varkala <raj@mywavia.com>
2023-04-07 17:13:35 +0530

    customer io connector

---
+++ valmi-integrations/connectors/destination-customer-io/docker-entrypoint.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+python destination_container_wrapper.py  $VALMI_ENTRYPOINT "$@" 
+
---
+++ valmi-integrations/connectors/destination-customer-io/setup.py
@@ -0,0 +1,24 @@
+from setuptools import find_packages, setup
+
+MAIN_REQUIREMENTS = [
+    "airbyte-cdk",
+    "requests",
+    "pygsheets==2.0.5",
+    "google-auth-oauthlib==0.5.1",
+    "google-api-python-client==2.47.0",
+]
+
+TEST_REQUIREMENTS = ["pytest~=6.2"]
+
+setup(
+    name="destination_webhook",
+    description="Destination implementation for Webhook.",
+    author="valmi.io",
+    author_email="contact@valmi.io",
+    packages=find_packages(),
+    install_requires=MAIN_REQUIREMENTS,
+    package_data={"": ["*.json"]},
+    extras_require={
+        "tests": TEST_REQUIREMENTS,
+    },
+)
