eddbe6a4017329c7aefcbb08ebd2e802029a443d
Rajashekar Varkala <raj@mywavia.com>
2023-05-01 13:04:38 +0530

    stripe connector

--- valmi-integrations/connectors/destination-stripe/setup.py
+++ valmi-integrations/connectors/destination-stripe/setup.py
@@ -5,7 +5,7 @@ MAIN_REQUIREMENTS = ["valmi_connector_lib", "requests"]
 TEST_REQUIREMENTS = ["pytest~=6.2"]
 
 setup(
-    name="destination_Stripe",
+    name="destination_stripe",
     description="Destination implementation for Stripe .",
     author="valmi.io",
     author_email="contact@valmi.io",
