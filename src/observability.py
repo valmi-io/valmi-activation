41a7ab283e0668e7790e898438c447b7ad077a04
Rajashekar Varkala <raj@mywavia.com>
2023-08-11 23:39:12 +0530

    duplicate logs removed

--- src/observability.py
+++ src/observability.py
@@ -24,7 +24,8 @@ from opentelemetry.sdk.metrics.export import PeriodicExportingMetricReader
 from opentelemetry.instrumentation.fastapi import FastAPIInstrumentor
 
 from fastapi import FastAPI
-
+import os
+from vyper import v
 
 def observable_counter_func(options: CallbackOptions) -> Iterable[Observation]:
     yield Observation(1, {})
@@ -46,24 +47,36 @@ def setup_observability(app: FastAPI):
     ########
     # Logs # - OpenTelemetry Logs are still in the "experimental" state, so function names may change in the future
     ########
-    logging.basicConfig(level=logging.DEBUG)
-
+    
     # Initialize logging and an exporter that can send data to an OTLP endpoint by attaching OTLP handler to root logger
     _logs.set_logger_provider(LoggerProvider())
-    # You can add handlers to custom loggers as well
-    logging.getLogger().addHandler(
-        LoggingHandler(
-            logger_provider=_logs.get_logger_provider().add_log_record_processor(
-                BatchLogRecordProcessor(OTLPLogExporter())
-            )
+
+    format_str = os.environ.get('OTEL_PYTHON_LOG_FORMAT',
+                                "%(asctime)s %(levelname)s [%(name)s] [%(filename)s:%(lineno)d] [trace_id=%(otelTraceID)s span_id=%(otelSpanID)s resource.service.name=%(otelServiceName)s trace_sampled=%(otelTraceSampled)s] - %(message)s")
+
+    _logs.set_logger_provider(LoggerProvider())
+    '''
+    # TODO: This is not working. Check this. The root handler is duplicating the logs. Disabled root hander.
+
+    # Adding Handler to root logger
+    root_log_handler = LoggingHandler(
+        logger_provider=_logs.get_logger_provider().add_log_record_processor(
+            BatchLogRecordProcessor(OTLPLogExporter())
         )
     )
+    root_log_handler.setFormatter(format_str)
+    logging.getLogger().addHandler(
+        root_log_handler
+    )
+    '''
 
-    # Adding Handler to activation.engine_api
-    logging.getLogger("activation.engine_api").addHandler(
-        LoggingHandler(
-            logger_provider=_logs.get_logger_provider().add_log_record_processor(
-                BatchLogRecordProcessor(OTLPLogExporter())
-            )
+    # Adding Handler to core.engine_api
+    engine_api_log_handler = LoggingHandler(
+        logger_provider=_logs.get_logger_provider().add_log_record_processor(
+            BatchLogRecordProcessor(OTLPLogExporter())
         )
+    )
+    engine_api_log_handler.setFormatter(logging.Formatter(format_str))
+    logging.getLogger(v.get("LOGGER_NAME")).addHandler(
+        engine_api_log_handler
     )
