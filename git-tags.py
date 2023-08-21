e671f751bca6e11afe7567482c5f43c4a4637a6c
Rajashekar Varkala <raj@mywavia.com>
2023-08-21 23:31:35 +0530

    tags updated for destinations to get latest valmi-connector-lib

--- git-tags.py
+++ git-tags.py
@@ -1,22 +1,22 @@
 import os
 connectors = {
-    "destination-webhook": "0.1.9",
-    "destination-stripe": "0.1.4",
-    "destination-slack": "0.1.4",
-    "destination-hubspot": "0.1.4",
-    "destination-google-sheets": "0.1.4",
-    "destination-google-ads": "0.1.4",
-    "destination-gong": "0.1.4",
-    "destination-facebook-ads": "0.1.4",
-    "destination-customer-io": "0.1.4",
-    "destination-android-push-notifications": "0.1.5",
+    "destination-webhook": "0.1.10",
+    "destination-stripe": "0.1.5",
+    "destination-slack": "0.1.5",
+    "destination-hubspot": "0.1.5",
+    "destination-google-sheets": "0.1.5",
+    "destination-google-ads": "0.1.5",
+    "destination-gong": "0.1.5",
+    "destination-facebook-ads": "0.1.5",
+    "destination-customer-io": "0.1.5",
+    "destination-android-push-notifications": "0.1.6",
                                         
-    "source-redshift": "0.1.7",
-    "source-snowflake": "0.1.7",
-    "source-postgres": "0.1.7"
+    "source-redshift": "0.1.8",
+    "source-snowflake": "0.1.8",
+    "source-postgres": "0.1.8"
 }
 
-valmi_core = "0.1.12"
+valmi_core = "0.1.13"
 
 
 def tag_and_push(tag_name, message):
